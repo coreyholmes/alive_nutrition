40ce7137e2fad3cd15491970ffbf4f446bb8c786
Corey Holmes <coreyholmes20@gmail.com>
2016-02-17 12:18:09 -0800

    First Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def product
+  end
+
+  def research
+  end
+
+  def faq
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get product" do
+    get :product
+    assert_response :success
+  end
+
+  test "should get research" do
+    get :research
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+end
